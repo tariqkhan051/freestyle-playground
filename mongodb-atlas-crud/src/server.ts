d7d5128c27395c9011f73dc6f16976c572a2f080
Tariq Khan <tariqkhan051@outlook.com>
2024-01-15 20:15:01 +0400

    add jest tests

--- mongodb-atlas-crud/src/server.ts
+++ mongodb-atlas-crud/src/server.ts
@@ -1,28 +1,8 @@
-import express from 'express';
-import dotenv from 'dotenv';
-
-import { run } from './database-connection';
-import { roleRoute } from './routes/role.route';
-import { userRoute } from './routes/user.route';
-import { listingRoute } from './routes/listing.route';
-
-dotenv.config();
+import app from "./app";
+import { run } from "./database-connection";
 
 const HOST = process.env.HOST || 'http://localhost';
 const PORT = parseInt(process.env.PORT || '4500');
-
-const app = express();
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-app.use('/', roleRoute());
-app.use('/', userRoute());
-app.use('/', listingRoute());
-
-app.get('/', (req, res) => {
-    return res.json({ message: 'Hello World!' });
-});
 
 app.listen(PORT, async () => {
     await run().catch(console.dir);
